5790a5f4a1ebf74250163c1cd2cd7c07c6e81173
Frank Paul Silye <frankps@gmail.com>
2026-04-03 20:21:28 +0200

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/harbour-dee-nb_NO.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>CommunitiesPage</name>
+    <message>
+        <source>Communities</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You aren&apos;t following any communities</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LemmyAPI</name>
+    <message>
+        <source>Please fill in all fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login succeeded but no token received</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>https://lemmy.world</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Logging in...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Email or Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PostPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Share url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo upvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Downvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo downvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SubscribedPage</name>
+    <message>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Communities</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No posts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo upvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Downvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo downvote</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
